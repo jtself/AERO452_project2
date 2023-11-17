096d3ec5a8cf575a1043dc4895c4257052fb8159
Justin <jtself@calpoly.edu>
2023-11-17 13:53:21 -0800

    Added main script to GIT

---
+++ A452_Project2_Script.m
@@ -0,0 +1,16 @@
+%{
+AERO452 | SPACEFLIGHT DYNAMICS II
+Group Project #2
+Authors: 
+    Travis Bouck
+    Justin Self
+Due date: 
+    Dec. 8, 2023
+%}
+
+% Housekeeping
+clear all; close all; clc;
+
+%% Section 1
+
+%% Section 2

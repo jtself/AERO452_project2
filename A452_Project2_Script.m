07ba76afa3a2e644f2ed758e2e3cb66c505c6cd6
TravisBouck1 <tbouck@calpoly.edu>
2023-12-04 15:26:56 -0800

    Part 1,2 dont. Part 3 almost....

--- A452_Project2_Script.m
+++ A452_Project2_Script.m
@@ -429,91 +429,260 @@ tm = -1;
 overallTime = [];
 overallState = [];
 overallDeltaV = [];
+initalPoints = [];
+finalPoints = [];
+initialTime = [];
+finalTime = [];
 init_pert = [h; ecc; TA; raan ; inc ;w]; 
 init_noPert = [h; ecc; TA; raan ;inc ;w];
 init = [init_pert;init_noPert];
 options = odeset('RelTol', 1e-12, 'AbsTol',1e-12,'Events',@eventDeOrbit);
-tfinal = 24*3600;
+
+
+perTime = 1.35*24*3600;
+tfinal = perTime;
+
 tspan = [0 tfinal]; 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-for i = linspace(1,20,20)
+for i = 1:5
 
 [time, state] = ode45(@vop_ODE_Part2, tspan, init, options,wEarth, re, mu, muSun, Cd, area, mass, SC.init.jd, Cr, Psr); 
 
-[r1vec,v1] = COES2RandV(state((length(state)-50),1)...
-    ,state((length(state)-50),2)...
-    ,state((length(state)-50),5)...
-    ,state((length(state)-50),4)...
-    ,state((length(state)-50),6)...
-    ,state((length(state)-50),3)...
+
+for j = 1:length(state)
+    if   time(end)-time(length(state)-j) > 500
+        dt = time(end)-time(length(state)-j);
+        break
+    end
+end
+
+
+[r1vec,v1] = COES2RandV(state((length(state)-j),1)...
+    ,state((length(state)-j),2)...
+    ,state((length(state)-j),5)...
+    ,state((length(state)-j),4)...
+    ,state((length(state)-j),6)...
+    ,state((length(state)-j),3)...
     ,mu);
 
 [r2vec,v2] = COES2RandV(state(end,7),state(end,8),state(end,11),state(end,10),state(end,12),state(end,9),mu);
 
-dt = time(end)-time(length(state)-50); 
 [v1vec, v2vec] = lambert(r1vec, r2vec, dt, tm, mu);
 
 deltaV = norm(v1-v1vec) + norm(v2-v2vec);
 
-overallDeltaV = [overallDeltaV;deltaV];
-overallTime = [overallTime;time + 24*3600*i - 24*3600];
-overallState = [overallState;state];
+time = time + perTime*i - perTime;
 
-init_pert = [state(end,7);state(end,8);state(end,9);state(end,10);state(end,11);state(end,12)]; 
+overallDeltaV = [overallDeltaV;deltaV];
+overallTime   = [overallTime;time];
+overallState  = [overallState;state];
+
+init_pert   = [state(end,7);state(end,8);state(end,9);state(end,10);state(end,11);state(end,12)];
 init_noPert = [state(end,7);state(end,8);state(end,9);state(end,10);state(end,11);state(end,12)]; 
-init = [init_pert;init_noPert];
+
+
+finalPertLocation = [state(end,1);state(end,2);state(end,3);state(end,4);state(end,5);state(end,6)];
+initPertLocation  = [state(1,1);state(1,2);state(1,3);state(1,4);state(1,5);state(1,6)];
+
+init         = [init_pert;init_noPert];
+initalPoints = [initalPoints;initPertLocation'];
+initialTime  = [initialTime;time(1)];
+finalPoints  = [finalPoints;finalPertLocation'];
+finalTime    = [finalTime;time(end)];
+
 
 end
 
 overallTime = overallTime/24/3600;
+initialTime = initialTime/24/3600;
+finalTime= finalTime/24/3600;   
 
 figure
-subplot(1,2,1)
 plot(overallTime,overallState(:,1)- SC.init.h)
-subplot(1,2,2)
-plot(overallTime,overallState(:,7)- SC.init.h)
+hold on
+plot(overallTime,overallState(:,7)- SC.init.h,'LineWidth',2)
+plot(initialTime,initalPoints(:,1)-SC.init.h,'*','LineWidth',3)
+plot(finalTime,finalPoints(:,1)-SC.init.h,'*','LineWidth',3)
+ylabel("h-h0 [km2/s]")
+xlabel("Time [days]")
+grid on
+legend("Perturbed Orbit","Osculating Orbit","Initial change in h","Final change in h",'Location','best')
+
+figure
+plot(overallTime,overallState(:,2)-SC.init.ecc)
+hold on
+plot(overallTime,overallState(:,8)-SC.init.ecc,'LineWidth',2)
+plot(initialTime,initalPoints(:,2)-SC.init.ecc,'*','LineWidth',3)
+plot(finalTime,finalPoints(:,2)-SC.init.ecc,'*','LineWidth',3)
+ylabel("ecc-ecc0")
+xlabel("Time [days]")
+grid on
+legend("Perturbed Orbit","Osculating Orbit","Initial change in ecc","Final change in ecc",'Location','best')
+
+figure
+plot(overallTime,rad2deg(overallState(:,4)-SC.init.raan))
+hold on
+plot(overallTime,rad2deg(overallState(:,10)-SC.init.raan),'LineWidth',2)
+plot(initialTime,rad2deg(initalPoints(:,4)-SC.init.raan),'*','LineWidth',3)
+plot(finalTime,rad2deg(finalPoints(:,4)-SC.init.raan),'*','LineWidth',3)
+ylabel("raan-raan0 [degs]")
+xlabel("Time [days]")
+grid on
+legend("Perturbed Orbit","Osculating Orbit","Initial change in raan","Final change in raan",'Location','best')
+
+figure
+plot(overallTime,rad2deg(overallState(:,5)-SC.init.inc))
+hold on
+plot(overallTime,overallState(:,11)-SC.init.inc,'LineWidth',2)
+plot(initialTime,rad2deg(initalPoints(:,5)-SC.init.inc),'*','LineWidth',3)
+plot(finalTime,rad2deg(finalPoints(:,5)-SC.init.inc),'*','LineWidth',3)
+ylabel("inc-inc0 [degs]")
+xlabel("Time [days]")
+grid on
+legend("Perturbed Orbit","Osculating Orbit","Initial change in inc","Final change in inc",'Location','best')
+
+figure
+plot(overallTime,rad2deg(overallState(:,6)-SC.init.w))
+hold on
+plot(overallTime,rad2deg(overallState(:,12)-SC.init.w),'LineWidth',2)
+plot(initialTime,rad2deg(initalPoints(:,6)-SC.init.w),'*','LineWidth',3)
+plot(finalTime,rad2deg(finalPoints(:,6)-SC.init.w),'*','LineWidth',3)
+ylabel("w-w0 [degs]")
+xlabel("Time [days]")
+grid on
+legend("Perturbed Orbit","Osculating Orbit","Initial change in w","Final change in w",'Location','best')
+
+
+
+for i = 1:length(overallState)
+    [r_temp,v_temp] = COES2RandV(overallState(i,1),overallState(i,2),overallState(i,5),overallState(i,4),overallState(i,6),overallState(i,3),mu);
+    r(i,1:3) = r_temp;
+    v(i,1:3) = v_temp;
+    h = overallState(i,1);
+    ecc = overallState(i,2);
+    a = (h^2)/(mu*(1-ecc^2));
+    ra(i) = a + a*ecc;
+    rp(i) = 2*a - ra(i);
+end
+
+figure
+plot(overallTime,ra-re,'LineWidth',2)
+hold on
+plot(overallTime,rp-re,'LineWidth',2)
+grid on
+legend("Apogee","Perigee",'Location','best')
+ylabel("Altitude [km]")
+xlabel("Time [Days]")
+
+%% Section 3: Special Orbit
+
+
+% TLE Input (Envisat)
+SC.init.UTC = "12/04/2023 12:16:40"; % UTC
+SC.init.ecc = 0.0001316; % actual ecc but we must change it to zero
+SC.init.inc = deg2rad(98.2559);               % rad
+SC.init.rp = 762 + re;                 % km
+SC.init.ra = 764 + re;                 % km
+SC.init.raan = deg2rad(301.7273);            % rad
+SC.init.w = deg2rad(83.6424);               % rad
+SC.init.revperday = 14.38460028;              % rev/day
+SC.init.Me = deg2rad(29.2718);              % rad
+
+% Calculate Additional COEs
+SC.init.a = 0.5*(SC.init.rp + SC.init.ra);             % km
+SC.init.T = ( (2*pi) / (sqrt(mu)) ) * SC.init.a^(3/2); % sec
+SC.init.n = SC.init.revperday*(2*pi/(24*60*60));       % rad/sec
+SC.init.jd = juliandate(datetime(SC.init.UTC,"Format","MM/dd/uuuu HH:mm:ss"));
+SC.init.E = newtonsKepler(SC.init.Me, SC.init.ecc);
+SC.init.TA = 2*atan((tan(SC.init.E/2))/(sqrt((1 - SC.init.ecc)/(1 + SC.init.ecc))));
+SC.init.h = findh(SC.init.a, mu, SC.init.ecc);
+SC.init.h = sqrt(SC.init.rp*mu*(1 + SC.init.ecc* cos(SC.init.TA)));
+
+
+% Determine epoch r and v
+[SC.init.rVect,SC.init.vVect] = COES2RandV(SC.init.h,SC.init.ecc,...
+    SC.init.inc,SC.init.raan,SC.init.w,SC.init.TA,mu);
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% Need to model EXP Drag, J2-J6, Sun as extra Body, and SRP
+
+diameter = sqrt(0.1^2 + 0.1^2);        % m
+mass = 8211;            % kg
+area = (1/(1000^2))*26*10; % km2
+Cd = 2.2;
+Cr = 1.2;
+Psr = 4.57*10^-6;
+
+tfinal = (1)*24*3600; % RANDOM 
+tspan = [0 tfinal]; 
+options = odeset('RelTol', 1e-12, 'AbsTol',1e-12,'Events',@eventDeOrbit);
+init = [SC.init.h; SC.init.ecc; SC.init.TA; SC.init.raan ;SC.init.inc ;SC.init.w]; 
+[time, state] = ode45(@vop_ODE, tspan, init, options,wEarth, re, mu, muSun, Cd, area, mass, SC.init.jd, Cr, Psr); 
+
+time = time/(24*3600);
+% Find rVector
+r = zeros(length(state),3);
+for i = 1:length(state)
+    [r_temp,v_temp] = COES2RandV(state(i,1),state(i,2),state(i,5),state(i,4),state(i,6),state(i,3),mu);
+    r(i,1:3) = r_temp;
+    v(i,1:3) = v_temp;
+    h = state(i,1);
+    ecc = state(i,2);
+    a = (h^2)/(mu*(1-ecc^2));
+    ra(i) = a + a*ecc;
+    rp(i) = 2*a - ra(i);
+end
+
+figure
+h1 = gca;
+earth_sphere(h1)
+hold on
+plot3(r(:,1),r(:,2),r(:,3),'.')
+plot3(r(1,1),r(1,2),r(1,3),'*','LineWidth',5)
+plot3(r(end,1),r(end,2),r(end,3),'*','LineWidth',5)
+lgd = legend("Earth","Orbital Path","Start Position","End Position",'Location','southoutside');
+lgd.NumColumns = 2;
+xlabel("X [Km]")
+ylabel("Y [Km]")
+zlabel("Z [Km]")
+
+figure
+plot(time,ra-re,'LineWidth',2)
+hold on
+plot(time,rp-re,'LineWidth',2)
+grid on
+legend("Apogee","Perigee",'Location','best')
+ylabel("Altitude [km]")
+xlabel("Time [Days]")
+
+figure
+plot(time,state(:,1)- SC.init.h)
 ylabel("h-h0 [km2/s]")
 xlabel("Time [days]")
 grid on
 figure
-subplot(1,2,1)
-plot(overallTime,overallState(:,2)-SC.init.ecc)
-subplot(1,2,2)
-plot(overallTime,overallState(:,8)-SC.init.ecc)
+plot(time,state(:,2)-SC.init.ecc)
 ylabel("ecc-ecc0")
 xlabel("Time [days]")
 grid on
 figure
-subplot(1,2,1)
-plot(overallTime,rad2deg(overallState(:,3)-SC.init.TA))
-subplot(1,2,2)
-plot(overallTime,rad2deg(overallState(:,9)-SC.init.TA))
+plot(time,rad2deg(state(:,3)-SC.init.TA))
 ylabel("theta-theta0 [degs]")
 xlabel("Time [days]")
 grid on
 figure
-subplot(1,2,1)
-plot(overallTime,rad2deg(overallState(:,4)-SC.init.raan))
-subplot(1,2,2)
-plot(overallTime,rad2deg(overallState(:,10)-SC.init.raan))
+plot(time,rad2deg(state(:,4)-SC.init.raan))
 ylabel("raan-raan0 [degs]")
 xlabel("Time [days]")
 grid on
 figure
-subplot(1,2,1)
-plot(overallTime,overallState(:,5)-SC.init.inc)
-subplot(1,2,2)
-plot(overallTime,overallState(:,11)-SC.init.inc)
+plot(time,state(:,5)-SC.init.inc)
 ylabel("inc-inc0 [degs]")
 xlabel("Time [days]")
 grid on
 figure
-subplot(1,2,1)
-plot(overallTime,rad2deg(overallState(:,6)-SC.init.w))
-subplot(1,2,2)
-plot(overallTime,rad2deg(overallState(:,12)-SC.init.w))
+plot(time,rad2deg(state(:,6)-SC.init.w))
 ylabel("w-w0 [degs]")
 xlabel("Time [days]")
 grid on
-
